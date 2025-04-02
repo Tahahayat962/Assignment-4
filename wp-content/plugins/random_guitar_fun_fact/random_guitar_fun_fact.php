9862c10e1c4b6bb2dcee1ca4de1e08e6f62fa807
Tahahayat962 <taha.hayat@gmail.com>
2025-04-02 18:51:03 -0400

    Update random_guitar_fun_fact.php

--- wp-content/plugins/random_guitar_fun_fact/random_guitar_fun_fact.php
+++ wp-content/plugins/random_guitar_fun_fact/random_guitar_fun_fact.php
@@ -9,10 +9,6 @@ Author: Taha Hayat
 function random_guitar_fun_fact() {
     $facts = array(
         "The first electric guitar was created in 1931 by George Beauchamp.",
-        "The longest guitar solo ever played lasted over 27 hours!",
-        "Jimi Hendrix played a guitar that was left-handed, but he strung it upside down.",
-        "The world’s largest guitar was built in 2015 and measured 43 feet long!",
-        "A standard electric guitar has 6 strings, but you can find guitars with 7, 8, or even 12 strings!",
         "It is good practice to change your guitar strings every 2 months!",
     );
 
